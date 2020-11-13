7fdc84706cfb2e24db76f5149309398dc34eae5b
Meredith McIntosh <meredith@mcintosh.io>
2020-11-13 21:43:44 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -6,8 +6,8 @@ def deal_card
   rand(1..11)
 end
 
-def display_card_total(the_card_total)
-  puts "Your cards add up to #{the_card_total}"
+def display_card_total(card_total)
+  puts "Your cards add up to #{card_total}"
 end
 
 def prompt_user
@@ -18,8 +18,8 @@ def get_user_input
   gets.chomp
 end
 
-def end_game(the_card_total)
-  puts "Sorry, you hit #{the_card_total}. Thanks for playing!"
+def end_game(card_total)
+  puts "Sorry, you hit #{card_total}. Thanks for playing!"
 end
 
 def initial_round
@@ -60,4 +60,4 @@ def runner
   end
   end_game(card_total)
 end
-    
+runner
